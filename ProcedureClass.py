9cf1a52447fc591093c939d5b86a83e5df51b13d
Reeeb Alebamber <reese.alexander99@gmail.com>
2022-02-23 15:23:20 -0600

    homework chekc

--- ProcedureClass.py
+++ ProcedureClass.py
@@ -1,5 +1,5 @@
 class Procedure:
-    def __init__(self,procName,opDate,Doc,cost,patID):
+    def __init__(self, procName, opDate, Doc, cost, patID):
         self.__procedurename = procName
         self.__dateOfProcedure = opDate
         self.__Practitioner = Doc
@@ -8,7 +8,7 @@ class Procedure:
 
     def get_patientID(self):
         return self.__patID
-        
+
     def get_procName(self):
         return self.__procedurename
 
